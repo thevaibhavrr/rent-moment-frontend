b9273d04412e29ed4f7f8800f7c806d1c9d853b2
Vaibhav <vaibhav@Sumits-MacBook-Pro-2.local>
2025-08-26 11:45:10 +0530

    update url

--- app/services/api.ts
+++ app/services/api.ts
@@ -22,7 +22,7 @@ class ApiService {
     // Use localhost for development, deployed URL for production
     this.baseURL = process.env.NODE_ENV === 'production' 
       // ? 'https://cloth-backend-tpce.onrender.com/api'
-      ? 'https://rent-moment-backend.onrender.com//api'
+      ? 'https://rent-moment-backend.onrender.com/api'
       : 'http://localhost:5000/api';
  
     // this.baseURL = 'https://cloth-backend-tpce.onrender.com/api';
